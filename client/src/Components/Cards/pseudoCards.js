c7b0cc5d6ba07bebe7d71b08f075269da12f9eb6
KlaraGajaszek <klara.gajaszek@student.po.edu.pl>
2020-06-28 22:45:45 +0200

    fix Card

--- client/src/Components/Cards/pseudoCards.js
+++ client/src/Components/Cards/pseudoCards.js
@@ -1,17 +1,17 @@
-import img1 from '../../assets/memy_cards/1.jpg';
-import img2 from '../../assets/memy_cards/2.jpg';
-import img3 from '../../assets/memy_cards/3.jpg';
-import img4 from '../../assets/memy_cards/4.jpg';
-import img5 from '../../assets/memy_cards/5.jpg';
-import img6 from '../../assets/memy_cards/6.jpg';
-import img7 from '../../assets/memy_cards/7.jpg';
-import img8 from '../../assets/memy_cards/8.jpg';
-import img9 from '../../assets/memy_cards/9.jpg';
-import img10 from '../../assets/memy_cards/10.jpg';
-import img11 from '../../assets/memy_cards/11.jpg';
-import img12 from '../../assets/memy_cards/12.jpg';
-import img13 from '../../assets/memy_cards/13.jpg';
-import img14 from '../../assets/memy_cards/14.jpg';
+import img1 from 'assets/memy_cards/1.jpg';
+import img2 from 'assets/memy_cards/2.jpg';
+import img3 from 'assets/memy_cards/3.jpg';
+import img4 from 'assets/memy_cards/4.jpg';
+import img5 from 'assets/memy_cards/5.jpg';
+import img6 from 'assets/memy_cards/6.jpg';
+import img7 from 'assets/memy_cards/7.jpg';
+import img8 from 'assets/memy_cards/8.jpg';
+import img9 from 'assets/memy_cards/9.jpg';
+import img10 from 'assets/memy_cards/10.jpg';
+import img11 from 'assets/memy_cards/11.jpg';
+import img12 from 'assets/memy_cards/12.jpg';
+import img13 from 'assets/memy_cards/13.jpg';
+import img14 from 'assets/memy_cards/14.jpg';
 
 export const pseudoCards = [
   {
