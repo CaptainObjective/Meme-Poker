1c15ee8eb9e184c4b4431da248a765657298bf63
Maciej Kwietniewski <maciek.kwiet36@gmail.com>
2020-07-11 01:18:35 +0200

    mongoose-morgan

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,6 +7,8 @@ import * as cookieParser from 'cookie-parser';
 import { socketController } from './routes/socket';
 import { router } from 'routes';
 
+const morgan = require('mongoose-morgan');
+
 dotenv.config();
 
 const app = express();
@@ -31,3 +33,20 @@ server.listen(port, () => console.log(`Server listening on port ${port}`));
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(router);
+
+app.use(
+  morgan(
+    {
+      collection: 'error_logger',
+      connectionString: dbKey?.toString(),
+      // user: 'admin',
+      // pass: 'pass',
+    },
+    {
+      skip: function (req: Request, res: Response) {
+        return res.status < 400;
+      },
+    },
+    'dev'
+  )
+);
