6063ab92731135cd73d7d028d32427310ceb729e
Tom-Bul <t.bulat99@gmail.com>
2020-07-26 22:30:42 +0200

    added Error component

--- client/src/Components/ErrorBox/ErrorBox.jsx
+++ client/src/Components/ErrorBox/ErrorBox.jsx
@@ -1,33 +1,50 @@
-import React, { useState } from 'react'
-import { Button, Snackbar, IconButton } from '@material-ui/core'
+import React, { useState, useEffect } from 'react'
+import { Snackbar, IconButton } from '@material-ui/core'
 import MuiAlert from '@material-ui/lab/Alert';
 import CloseIcon from '@material-ui/icons/Close'
+import { useSocket } from 'socketio-hooks';
 
 const ErrorBox = props => {
+    const [snackPack, setSnackPack] = useState([]);
     const [open, setOpen] = useState(false);
+    const [error, setError] = useState(undefined);
 
-    const handleClick = () => {
-        setOpen(true);
-    };
+    useEffect(() => {
+        if (snackPack.length && !error) {
+            setError({ ...snackPack[0] });
+            setSnackPack((prev) => prev.slice(1));
+            setOpen(true);
+        } else if (snackPack.length && error && open) {
+            setOpen(false);
+        }
+    }, [snackPack, error, open]);
 
     const handleClose = (event, reason) => {
         if (reason === 'clickaway') {
             return;
         }
-
         setOpen(false);
     };
+    const handleExited = () => {
+        setError(undefined);
+    };
+
+    useSocket('EXCEPTION', message => {
+        setSnackPack((prev) => [...prev, { message, key: new Date().getTime() }]);
+    })
 
     return (
         <div>
             <Snackbar
+                key={error ? error.key : undefined}
                 anchorOrigin={{
                     vertical: 'bottom',
-                    horizontal: 'center',
+                    horizontal: 'left',
                 }}
                 open={open}
                 autoHideDuration={6000}
                 onClose={handleClose}
+                onExited={handleExited}
             >
                 <MuiAlert severity="error" variant="filled" elevation="6" onClose={handleClose} action={
                     <React.Fragment>
@@ -35,7 +52,7 @@ const ErrorBox = props => {
                             <CloseIcon fontSize="small" />
                         </IconButton>
                     </React.Fragment>}
-                >{props.content}</MuiAlert>
+                >{error ? error.message : undefined}</MuiAlert>
             </Snackbar>
         </div>
     )
